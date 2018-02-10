f7dc9f87643dde76b9efdc0e11bc367bc5c869c9
sh805190 <34245021+sh805190@users.noreply.github.com>
2018-02-10 23:02:53 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/chenenlin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chenenlin/myapplication/MainActivity.java
@@ -28,10 +28,10 @@ public class MainActivity extends AppCompatActivity {
                 String password = password1.getText().toString();
 
                 if(validatePasswordS2(password) == 5) {
-                    tv.setText("Password is strong enough!!!");
+                    tv.setText("Password is strong enough");
                 }
                 else{
-                    tv.setText("Password is not strong enough!!!");
+                    tv.setText("Password is not strong enough");
                 }
 
             }
@@ -40,44 +40,37 @@ public class MainActivity extends AppCompatActivity {
 
         //validate stage 1
     public static int validatePasswordS1(String password) {
-        int matchrules = 0 ;
+        int x = 0 ;
 
         if(password == null || password.isEmpty()) {
             return 0;
         }
-        // its not PASSWORD
-        if(!password.equalsIgnoreCase("PASSWORD")) {
-            matchrules++;
+        // its not ''password'
+        if(!password.equalsIgnoreCase("password")) {
+            x++;
         }
         // length >= 8
         if(password.length() >= 8 ) {
-            matchrules++;
+            x++;
         }
-        return matchrules;
-    }
-
-        //validate stage 2
-    public static int validatePasswordS2(String password) {
-        //sum up the count num
-        int matchcount =  validatePasswordS1(password);
-
+        return x;
         //check have special characters
         if(password.indexOf('!') != -1 || password.indexOf('#') != -1
                 || password.indexOf('@') != -1 || password.indexOf('%') != -1
                 || password.indexOf('$') != -1 || password.indexOf('*') != -1
                 || password.indexOf('^') != -1 || password.indexOf('&') != -1 ) {
-            matchcount++;
+            x++;
         }
 
         //check if string is at-least one digit
         if(Pattern.compile("[0-9]").matcher(password).find()){
-            matchcount++;
+            x++;
         }
 
         //check if string has both upper and lower case
         if(Pattern.compile("[a-z]").matcher(password).find() && Pattern.compile("[A-Z]").matcher(password).find()) {
-            matchcount++;
+            x++;
         }
-        return matchcount;
+        return x;
     }
 }
