ba2f61453436e3e8577e2b4a45f9b77a742cc474
sh805190 <34245021+sh805190@users.noreply.github.com>
2018-02-10 21:51:17 +0800

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -9,7 +9,7 @@ public class Validator {
 		if(password == null || password.isEmpty()) {
 			return 0;
 		}
-			// its not PASSWORD
+			// its not "password"
 		if(! password.equals("password")) {
 			x++;
 		}
@@ -21,5 +21,27 @@ public class Validator {
 		return x;	
 
 	}
+		public static int correctpassword2(String password) {
+		//sum up the count num
+		int x =  correctpassword2(password);
+		
+		//check have special characters
+		if(password.indexOf('!') != -1 || password.indexOf('#') != -1 
+				|| password.indexOf('@') != -1 || password.indexOf('%') != -1
+				|| password.indexOf('$') != -1 || password.indexOf('*') != -1
+				|| password.indexOf('^') != -1 || password.indexOf('&') != -1 ) {
+			x ++;
+		}
+		
+		//check if string is at-least one digit
+		if(Pattern.compile("[0-9]").matcher(password).find()){
+			x++;
+		}
+		
+		//check if string has both upper and lower case
+		if(Pattern.compile("[a-z]").matcher(password).find() && Pattern.compile("[A-Z]").matcher(password).find()) {		
+			x++;
+		}
+		return x;
 	
 }
